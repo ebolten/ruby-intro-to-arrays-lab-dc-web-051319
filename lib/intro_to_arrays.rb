7311c8f133002cf504e4bfda68b4d830265e7719
Emily Bolten <emilybolten@gmail.com>
2019-05-02 23:53:57 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,35 @@
+my_new_array = []
+
+my_two_array = ['hello','ciao']
+
+def instantiate_new_array()
+  return []
+end
+
+def array_with_two_elements()
+  return my_two_array = ['hello','ciao']
+end
+
+def first_element(array)
+  return array.first
+end
+
+def last_element(array)
+  return array[-1]
+end
+
+def last_element_with_array_methods(array)
+  return array.last
+end
+
+def third_element(array)
+  return array[2]
+end
+
+def length_of_array(array)
+  return array.size
+end
+
+def first_element_with_array_methods(array)
+  return array.first
+end
