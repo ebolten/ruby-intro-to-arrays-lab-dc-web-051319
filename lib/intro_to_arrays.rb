05542e23d516732f6f5572531d7b692b718b6afa
Emily Bolten <emilybolten@gmail.com>
2019-05-02 23:52:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -14,10 +14,18 @@ def first_element(array)
   return array.first
 end
 
-def last_element(array)
+def last_element_with_array_methods(array)
   return array.last
 end
 
 def third_element(array)
   return array[2]
+end
+
+def length_of_array(array)
+  return array.size
+end
+
+def first_element_with_array_methods(array,element)
+  return array.index(element)
 end
